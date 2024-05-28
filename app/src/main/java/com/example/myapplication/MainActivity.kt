98ad94761c101d286a847be836bd262805021ec3
송유호 <wjsghk7664@naver.com>
2024-05-28 17:21:45 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.joo).setOnClickListener {
+            val intent=Intent(this,joo::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.yoo).setOnClickListener {
+            val intent=Intent(this,yooho::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.jeong).setOnClickListener {
+            val intent=Intent(this,jeong::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.chan).setOnClickListener {
+            val intent=Intent(this,chan::class.java)
+            startActivity(intent)
+        }
+
+        findViewById<Button>(R.id.seong).setOnClickListener {
+            val intent=Intent(this,seong::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/yooho.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
+class yooho : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yooho)
+
+        val toolbar = findViewById<Toolbar>(R.id.mainToolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_home)
+
+
+        toolbar.title="\"I\"송유호 소개페이지"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item!!.itemId){
+            android.R.id.home -> finish()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
