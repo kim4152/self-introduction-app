bb8cbbd4a17849e3ce42704428fd9f53cd330348
hamond12 <chicken010208@gmail.com>
2024-05-29 20:41:04 +0900

    송주영 자기소개 페이지 작성 완료

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
