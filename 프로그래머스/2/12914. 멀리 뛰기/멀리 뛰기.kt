c56518cedcb9fd2e8de323ee940ce30e76d2ff82
kim4152 <108558921+kim4152@users.noreply.github.com>
2024-06-11 15:03:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.09 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(n: Int): Long {
+    if (n == 1) return 1
+    if (n == 2) return 2
+
+    val mod = 1234567
+    var a = 1L
+    var b = 2L
+
+    for (i in 3..n) {
+        val c = (a + b) % mod
+        a = b
+        b = c
+    }
+
+    return b
+}
+}
